98fcbc194161fdf8d742e7393b9beaa9c9187058
Jarfsoft <andresjex@hotmail.com>
2021-04-15 11:06:18 -0600

    Print info in HTML, get info from input

--- src/getInfo.js
+++ src/getInfo.js
@@ -1,4 +1,4 @@
-async function getWeatherInfo(cityName = 'Tegucigalpa') {
+async function getWeatherInfo(cityName) {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1c954fe59dca8548d2eb13416b4f9da3`,
     { mode: "cors" }
   );
@@ -7,29 +7,25 @@ async function getWeatherInfo(cityName = 'Tegucigalpa') {
   return data;
 }
 
-export const returnInfo = city => {
-  const obj = {};
-  obj.city = city;
+export const returnInfo = (city = 'Tegucigalpa', metricSystem = true) => {
   getWeatherInfo(city).then(data => {
     const {sys: {country: ctr}} = data;
 
-    obj.country = ctr;
+    document.querySelector('#city_country').textContent = `${city}, ${ctr}`;
     
     const {weather} = data;
+    const [frst] = weather;
 
-    obj.status = weather[0].main;
-    obj.description = weather[0].description;
+    document.querySelector('#description').textContent = `${frst.main}, ${frst.description}`;
     
     const {main: {temp: tmp, feels_like: fl, humidity: hm}} = data;
 
-    obj.temperature = tmp;
-    obj.thermalSensation = fl;
-    obj.humidity = hm;
+    document.querySelector('#temperature').textContent = `${metricSystem? (tmp - 273.15).toFixed(2) : (((tmp - 273.15) * 9 / 5) + 32).toFixed(2)} °${metricSystem ? 'C' : 'F'}`;
+    document.querySelector('#thermalSensation').textContent = `Thermal Sensation: ${metricSystem? (fl - 273.15).toFixed(2) : (((fl - 273.15) * 9) / 5 + 32).toFixed(2)} °${metricSystem ? 'C' : 'F'}`;
+    document.querySelector('#humidity').textContent = `Humidity: ${hm}%`;
 
     const {wind: {speed: spd}} = data;
 
-    obj.windSpeed = spd;
+    document.querySelector('#windSpeed').textContent = `Wind Speed: ${spd}m/s`;
   });
-  
-  return obj;
 };
--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import {
   returnInfo
 } from './getInfo';
 
+let metricSystem = true;
 
-console.log(returnInfo('London'));
+const city = document.querySelector('#cityInput');
+
+document.querySelector('#metricSystem').addEventListener('click', () => {
+  metricSystem ? metricSystem = false : metricSystem = true;
+  returnInfo(city.value == '' ? 'Tegucigalpa' : city.value , metricSystem);
+});
+
+document.querySelector('#cityBtn').addEventListener('click', () => {
+  returnInfo(city.value, metricSystem);
+});
+
+returnInfo();
+
